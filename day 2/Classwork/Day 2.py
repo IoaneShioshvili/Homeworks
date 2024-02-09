70f9bc4a0e120e879197ca67108814d3cc2e66cf
IoaneShioshvili <ioaneshioshvili1@outlook.com>
2024-02-09 20:40:04 +0400

    commit

---
+++ day 2/Classwork/Day 2.py
@@ -0,0 +1,4 @@
+print('მე გავიცანი ნიკა მასწავლებელი დათა თეზელაშვილი და ძალიან ბევრი ბავშვი')
+
+
+print('chemi azrit goa aris saulekteso akademia goalshi dzalian kargi maswavlebelia mxolod mshromeli da kargi bavshvebi swavloben goalshi gaswavlin rogor unda gavxdet milionerebi rac yoveli chventaganis ocnebaa a aq dzalian mxiaruli da kargi gakvetilebi tardeba')
