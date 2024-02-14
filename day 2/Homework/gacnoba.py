32e4f61703fc291e8e0bbb331f01ef2456fb6aba
IoaneShioshvili <ioaneshioshvili1@outlook.com>
2024-02-14 13:29:11 +0400

    commit

---
+++ day 2/Homework/gacnoba.py
@@ -0,0 +1,6 @@
+gacnoba = "me gavicani data tezelashvili,dzalian kargi maswavlebelia"
+goa = "me goaze vpikrob, goa aris sauketeso akademia dzalian megobrulia " 
+keshelava = "nika maswavlebeli dzalian megobrulia,dzalian kargad xsnis yvelafers da shromis mowodebas gvadzlevs rac dzalian kargia"
+tezelashvili = "data maswavlebeli aris yuradgebiani rodesac rames ver gavieb im wamsve mixsnis "
+print(gacnoba + " " + goa)
+print(keshelava + " " + tezelashvili)
